7339536a58890366f400463edaec794e65d183ee
LoganTann <logon313@hotmail.fr>
2024-02-15 12:27:24 +0100

    fix: Breaking change on unit tests android 13+

--- app/src/androidTest/java/fr/spiderboy/navigoat/ApplicationTest.java
+++ app/src/androidTest/java/fr/spiderboy/navigoat/ApplicationTest.java
@@ -1,13 +1,21 @@
 package fr.spiderboy.navigoat;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import static org.junit.Assert.assertEquals;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("fr.spiderboy.navigoat", appContext.getPackageName());
     }
 }
